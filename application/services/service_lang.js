e437686f8d64882fe742ce2713dd024112ac984b
Changho Kang <inodient@gmail.com>
2018-05-30 16:58:34 +0900

    session controlling test

--- application/services/service_lang.js
+++ application/services/service_lang.js
@@ -1,3 +1,57 @@
+// exports.setDefaultLang = function( req, res ){
+//   return new Promise( function(resolve, reject){
+//     var connHandler = new connectionHandler( req, res );
+
+//     connHandler.getSession( "lang", function(result, err){
+//       if( err ) reject( err );
+
+//       if( result === "undefined" ){
+//         var lang = getBrowserDefaultLang( req );
+        
+//         connHandler.setSession( "lang", lang, function(){
+//           resolve( lang );
+//         } );
+
+//         resolve( lang );
+//       } else {
+//         resolve( result );
+//       }
+//     } );
+//   } );
+// }
+
+
+
+
+// exports.getLang = function( req, res ){
+//   var connHandler = new connectionHandler( req, res );
+
+//   connHandler.getSession( "lang", function(result, err){
+//     return result;
+//   } );
+// }
+
+
+
+
+// function getBrowserDefaultLang( req ){
+//   try{
+//     var lang = req.headers["accept-language"];
+
+//     logger.debug( "accept-language :", lang );
+
+
+//     lang = ( ( ( ( ( lang.split(";") )[0] ).split(",") )[0] ).split("-") )[0];
+//     return lang;
+//   } catch( err ){
+//     throw err;
+//   }
+// }
+
+
+
+
+
 exports.setDefaultLang = function( req, res ){
   return new Promise( function(resolve, reject){
     var connHandler = new connectionHandler( req, res );
@@ -26,6 +80,9 @@ exports.setDefaultLang = function( req, res ){
 
 exports.getLang = function( req, res ){
   var connHandler = new connectionHandler( req, res );
+
+  logger.debug( connHandler.getCookie( "lang" ) );
+
   return connHandler.getCookie( "lang" )
 }
 
--- application/views/js/kangchangho-com.js
+++ application/views/js/kangchangho-com.js
@@ -64,6 +64,49 @@ $(document).ready( function(){
     location.reload();
   } );
 
+  // if( !$("#lanuage_change_modal").hasClass("in") ){
+  //   if( $.cookie( "lang") != $("body").data("lang") ){
+
+  //     if( $.cookie("lang") === "ko" ){
+
+  //       $("#language_change_modal #language-change-message").html( 
+  //         '<b>언어 설정 변경</b>' + 
+  //         '<hr>' + 
+  //         '언어 설정을 한국어로 변경합니다.'
+  //       );
+        
+  //       $("#language_change_modal").modal("show");
+   
+  //     } else if( $.cookie("lang") === "en" ) {
+  //       $("#language_change_modal #language-change-message").html( 
+  //         '<b>Notification</b>' + 
+  //         '<hr>' + 
+  //         'Default language will be setted as English.'
+  //       );
+
+  //       $("#language_change_modal").modal("show");
+  //     } else {
+  //       document.cookie = "lang=" + $("body").data("lang") + ";path=/;" + ";";
+  //     }
+  //   }
+  // }
+
+  // if( $.cookie( "lang" ).indexOf( "ko" ) > -1 ){
+  //   $("#language-changer").text( "ko" );
+  // } else {
+  //   $("#language-changer").text( "en" );
+  // }
+
+  // $("#language-changer").on( "click", function(){
+  //   if( "ko" == $.cookie( "lang") ){
+  //     document.cookie = "lang=en;path=/;" + ";";
+  //   } else if( "en" == $.cookie( "lang") ){
+  //     document.cookie = "lang=ko;path=/;" + ";";
+  //   }
+
+  //   location.reload();
+  // } );
+
 
 
 
